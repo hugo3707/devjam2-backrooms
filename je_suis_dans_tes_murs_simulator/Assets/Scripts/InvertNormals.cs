632e7a6842d71f034703b59f7ef23b84ab0e534b
Kougo3655 <81871348+Kougo3655@users.noreply.github.com>
2022-06-18 17:08:36 +0200

    essai textures level_1

---
+++ je_suis_dans_tes_murs_simulator/Assets/Scripts/InvertNormals.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using System.Linq;
+public class InvertNormals : MonoBehaviour
+{
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
